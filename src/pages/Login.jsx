532358be4b02c20acb6dd9f7de92e9906ee353e7
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-02-25 20:25:53 +0200

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,43 @@
 import { useState } from "react";
 import axios from "axios";
+import { toast } from "react-hot-toast";
+import {useNavigate} from "react-router-dom";
 
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
 
 const Login = () => {
+    const navigate = useNavigate();
     const [data, setData] = useState({
         email: "",
         password: ""
     })
 
-    const loginUser = (e) => {
+    const loginUser = async (e) => {
         e.preventDefault();
-        const {email, password} = data;
+        const { email, password } = data;
         try {
-            
+            const { data } = await axios.post("/login", {
+                email,
+                password
+            });
+            if (data.error) {
+                toast.error(data.error);
+            }else {
+                setData({});
+                navigate("/");
+            }
         } catch (error) {
-            
+
         }
     }
 
     return (
         <form onSubmit={loginUser}>
             <label>Email</label>
-            <input type='email' placeholder='Enter your email...' value={data.email} onChange={(e) => setData({...data, email: e.target.value})}/>
+            <input type='email' placeholder='Enter your email...' value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
             <label>Password</label>
-            <input type='password' placeholder='Enter your password...' value={data.password} onChange={(e) => setData({...data, password: e.target.value})}/>
+            <input type='password' placeholder='Enter your password...' value={data.password} onChange={(e) => setData({ ...data, password: e.target.value })} />
             <button type='submit'>Login</button>
         </form>
     )
