165640e6c4aeb814d479f0744ad12b6b093fce39
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-02-26 13:41:21 +0200

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/register'>Register</Link>
-      <Link to='/login'>Login</Link>
+      <Link to='/' className="link">Home</Link>
+      <Link to='/register' className="link">Register</Link>
+      <Link to='/login' className="link">Login</Link>
     </nav>
   )
 }
